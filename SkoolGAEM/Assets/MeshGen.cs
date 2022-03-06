32bfb320c7dfd7b154bd72529c8bfbbe5e9e5d39
AlexD <80286400+Alexdlc5@users.noreply.github.com>
2022-03-05 21:46:27 -0800

    RandomMapFix
    Fixed issues with larger world generation
    applied a UV accurate texture to the ground

--- SkoolGAEM/Assets/MeshGen.cs
+++ SkoolGAEM/Assets/MeshGen.cs
@@ -4,32 +4,33 @@ using UnityEngine;
 
 public class MeshGen : MonoBehaviour
 {
+    //Map is 900x900 units with current settings
     Mesh mesh;
 
     Vector3[] vertices;
     int[] triangles;
+    Vector2[] UVs;
     
     public int width = 256;
     public int height = 256;
-    public float offsetx = 100f;
-    public float offsety = 100f;
+    public int offsetx = 100;
+    public int offsetz = 100;
     public float colordamp = 1;
     public float stretchx = 1;
     public float stretchy = 1;
     public float scale = 20;
+    public float scaleMultiplier = 1;
     public int xSize = 20;
     public int zSize = 20;
+    public GameObject vertmark;
     public float amp = 1;
 
     void Start()
     {
-        offsetx = Random.Range(0, 9999f);
-        offsety = Random.Range(0, 9999f);
-
+        offsetx = Random.Range(0, 999);
+        offsetz = Random.Range(0, 999);
         mesh = new Mesh();
         GetComponent<MeshFilter>().mesh = mesh;
-       
-        Renderer renderer = GetComponent<Renderer>();
 
         CreateShape();
         UpdateMesh();
@@ -39,21 +40,23 @@ public class MeshGen : MonoBehaviour
 
     void CreateShape()
     {
+        //creates grid of vertices
         vertices = new Vector3[(xSize + 1) * (zSize + 1)];
         
         int index = 0;
-        for (int i = 0; i <= zSize; i++)
+        for (int z = 0; z <= zSize; z++)
         {
-            for (int j = 0; j <= xSize; j++)
+            for (int x = 0; x <= xSize; x++)
             {
-                float xCoord = (float)j / width * scale + offsetx;
-                float yCoord = (float)i / height * scale + offsety;
-                float y = Mathf.PerlinNoise(xCoord, yCoord);
-                vertices[index] = new Vector3(j, y * amp, i);
+                float xCoord = (float)x / width * scale + offsetx;
+                float zCoord = (float)z / height * scale + offsetz;
+                float y = Mathf.PerlinNoise(xCoord, zCoord);
+                vertices[index] = new Vector3(x, y * amp, z);
                 index++;
             }
         }
         
+        //creates triangles/squares in grid
         triangles = new int[xSize * zSize * 6];
         int vert = 0;
         int tri = 0;
@@ -61,17 +64,29 @@ public class MeshGen : MonoBehaviour
         {
             for (int x = 0; x < xSize; x++)
             {
+                //first triangle
                 triangles[tri] = vert + 0;
                 triangles[tri + 1] = vert + xSize + 1;
                 triangles[tri + 2] = vert + 1;
-                triangles[tri + 3] = vert + 1;
-                triangles[tri + 4] = vert + xSize + 1;
+                //second triangle
+                triangles[tri + 3] = triangles[tri + 2];
+                triangles[tri + 4] = triangles[tri + 1];
                 triangles[tri + 5] = vert + xSize + 2;
 
                 vert++;
                 tri += 6;
             }
             vert++;
+        }
+
+        UVs = new Vector2[vertices.Length];
+        for (int i = 0, z = 0; z <= zSize; z++)
+        {
+            for(int x = 0; x <= xSize; x++)
+            {
+                UVs[i] = new Vector2((float)x / xSize,(float)z / zSize);
+                i++;
+            }
         }
     } 
 
@@ -81,7 +96,9 @@ public class MeshGen : MonoBehaviour
 
         mesh.vertices = vertices;
         mesh.triangles = triangles;
+        mesh.uv = UVs;
 
         mesh.RecalculateNormals();
+        transform.localScale = new Vector3(transform.localScale.x * scaleMultiplier, 1, transform.localScale.z * scaleMultiplier);
     }
 }
