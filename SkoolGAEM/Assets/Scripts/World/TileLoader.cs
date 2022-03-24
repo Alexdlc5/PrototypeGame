3ad88ef1be4d070fdc90e51e862ee0ae1c697f03
AlexD <80286400+Alexdlc5@users.noreply.github.com>
2022-03-23 20:53:25 -0700

    .6
    NEW TILE GEN WORKS!!!!!!!

--- SkoolGAEM/Assets/Scripts/World/TileLoader.cs
+++ SkoolGAEM/Assets/Scripts/World/TileLoader.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class TileLoader : MonoBehaviour
 {
     public GameObject tile;
-    List<Vector2> tilelocations = new List<Vector2>();
+    HashSet<Vector2> tilelocations = new HashSet<Vector2>();
     int tilecount = 0;
     private void Start()
     {
@@ -24,31 +24,17 @@ public class TileLoader : MonoBehaviour
         //checks for world gen hitboxes
         else if (other.gameObject.tag == "North")
         {
-            float tilecheck = tilecount;
             //current tile position
             Vector3 tilepos = other.GetComponentsInParent<Transform>()[1].position;
             Destroy(other.gameObject);
-            Debug.Log(tilepos.x.ToString() + ", " + tilepos.y.ToString() + ", " + tilepos.z.ToString());
+            //Debug.Log(tilepos.x.ToString() + ", " + tilepos.y.ToString() + ", " + tilepos.z.ToString());
             //new tile coords
             float xcoord = tilepos.x;
             float zcoord = tilepos.z + tileoffset;
-            //checks thru tilelocations to see if there is a tile in target position
-            for (int i = 0; i < tilecount; i++)
-            {
-                //if coords different
-                Debug.Log("X: " + xcoord + " : " + tilelocations[i].x);
-                Debug.Log("Z: " + zcoord + " : " + tilelocations[i].y);
-                if (xcoord != tilelocations[i].x || zcoord != tilelocations[i].y)
-                {
-                    tilecheck--;
-                } 
-                else
-                {
-                    break;
-                }
-            }
+            Vector2 newtileposvector = new Vector2(tilepos.x, tilepos.z + tileoffset);
+            
             //if all coords diffrent
-            if (tilecheck == 0)
+            if (!tilelocations.Contains(newtileposvector))
             {
                 //generate new tile
                 GameObject newtile = Instantiate(tile);
@@ -58,32 +44,17 @@ public class TileLoader : MonoBehaviour
         }
         else if (other.gameObject.tag == "South")
         {
-            //might not be set correctly
-            float tilecheck = tilecount;
             //current tile position
             Vector3 tilepos = other.GetComponentsInParent<Transform>()[1].position;
             Destroy(other.gameObject);
-            Debug.Log(tilepos.x.ToString() + ", " + tilepos.y.ToString() + ", " + tilepos.z.ToString());
+            //Debug.Log(tilepos.x.ToString() + ", " + tilepos.y.ToString() + ", " + tilepos.z.ToString());
             //new tile coords
             float xcoord = tilepos.x;
             float zcoord = tilepos.z - tileoffset;
-            //checks thru tilelocations to see if there is a tile in target position
-            for (int i = 0; i < tilecount; i++)
-            {
-                //if coords different
-                Debug.Log("X: " + xcoord + " : " + tilelocations[i].x);
-                Debug.Log("Z: " + zcoord + " : " + tilelocations[i].y);
-                if (xcoord != tilelocations[i].x || zcoord != tilelocations[i].y)
-                {
-                    tilecheck--;
-                } 
-                else
-                {
-                    break;
-                }
-            }
+            Vector2 newtileposvector = new Vector2(xcoord, zcoord);
+
             //if all coords diffrent
-            if (tilecheck == 0)
+            if (!tilelocations.Contains(newtileposvector))
             {
                 //generate new tile
                 GameObject newtile = Instantiate(tile);
@@ -91,66 +62,46 @@ public class TileLoader : MonoBehaviour
                 tilelocations.Add(new Vector2(xcoord, zcoord));
             }
         }
-        //else if (other.gameObject.tag == "East")
-        //{
-        //    float tilecheck = tilecount;
-        //    //current tile position
-        //    Vector3 tilepos = other.GetComponentsInParent<Transform>()[1].position;
-        //    Destroy(other.gameObject);
-        //    //Debug.Log(tilepos.x.ToString() + ", " + tilepos.y.ToString() + ", " + tilepos.z.ToString());
-        //    //new tile coords
-        //    float xcoord = tilepos.x + tileoffset;
-        //    float zcoord = tilepos.z;
-        //    //checks thru tilelocations to see if there is a tile in target position
-        //    for (int i = 0; i < tilecount; i++)
-        //    {
-        //        //if coords different
-        //        //Debug.Log("X: " + xcoord + " : " + tilelocations[i].x);
-        //        //Debug.Log("Z: " + zcoord + " : " + tilelocations[i].y);
-        //        if (xcoord != tilelocations[i].x || zcoord != tilelocations[i].y)
-        //        {
-        //            tilecheck--;
-        //        }
-        //    }
-        //    //if all coords diffrent
-        //    if (tilecheck == 0)
-        //    {
-        //        //generate new tile
-        //        GameObject newtile = Instantiate(tile);
-        //        newtile.GetComponent<Tile>().setTilePos(xcoord, zcoord);
-        //        tilelocations.Add(new Vector2(xcoord, zcoord));
-        //    }
-        //}
-        //else if (other.gameObject.tag == "West")
-        //{
-        //    float tilecheck = tilecount;
-        //    Destroy(other.gameObject);
-        //    //current tile position
-        //    Vector3 tilepos = other.GetComponentsInParent<Transform>()[1].position;
-        //    //Debug.Log(tilepos.x.ToString() + ", " + tilepos.y.ToString() + ", " + tilepos.z.ToString());
-        //    //new tile coords
-        //    float xcoord = tilepos.x - tileoffset;
-        //    float zcoord = tilepos.z;
-        //    //checks thru tilelocations to see if there is a tile in target position
-        //    for (int i = 0; i < tilecount; i++)
-        //    {
-        //        //if coords different
-        //        //Debug.Log("X: " + xcoord + " : " + tilelocations[i].x);
-        //        //Debug.Log("Z: " + zcoord + " : " + tilelocations[i].y);
-        //        if (xcoord != tilelocations[i].x || zcoord != tilelocations[i].y)
-        //        {
-        //            tilecheck--;
-        //        }
-        //    }
-        //    //if all coords diffrent
-        //    if (tilecheck == 0)
-        //    {
-        //        //generate new tile
-        //        GameObject newtile = Instantiate(tile);
-        //        newtile.GetComponent<Tile>().setTilePos(xcoord, zcoord);
-        //        tilelocations.Add(new Vector2(xcoord, zcoord));
-        //    }
-        //}
+        else if (other.gameObject.tag == "East")
+        {
+            //current tile position
+            Vector3 tilepos = other.GetComponentsInParent<Transform>()[1].position;
+            Destroy(other.gameObject);
+            //Debug.Log(tilepos.x.ToString() + ", " + tilepos.y.ToString() + ", " + tilepos.z.ToString());
+            //new tile coords
+            float xcoord = tilepos.x + tileoffset;
+            float zcoord = tilepos.z;
+            Vector2 newtileposvector = new Vector2(xcoord, zcoord);
+
+            //if all coords diffrent
+            if (!tilelocations.Contains(newtileposvector))
+            {
+                //generate new tile
+                GameObject newtile = Instantiate(tile);
+                newtile.GetComponent<Tile>().setTilePos(xcoord, zcoord);
+                tilelocations.Add(new Vector2(xcoord, zcoord));
+            }
+        }
+        else if (other.gameObject.tag == "West")
+        {
+            //current tile position
+            Vector3 tilepos = other.GetComponentsInParent<Transform>()[1].position;
+            Destroy(other.gameObject);
+            //Debug.Log(tilepos.x.ToString() + ", " + tilepos.y.ToString() + ", " + tilepos.z.ToString());
+            //new tile coords
+            float xcoord = tilepos.x - tileoffset;
+            float zcoord = tilepos.z;
+            Vector2 newtileposvector = new Vector2(xcoord, zcoord);
+
+            //if all coords diffrent
+            if (!tilelocations.Contains(newtileposvector))
+            {
+                //generate new tile
+                GameObject newtile = Instantiate(tile);
+                newtile.GetComponent<Tile>().setTilePos(xcoord, zcoord);
+                tilelocations.Add(new Vector2(xcoord, zcoord));
+            }
+        }
     }
     private void OnTriggerExit(Collider other)
     {
