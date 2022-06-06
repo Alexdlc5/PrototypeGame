891aca30119a9df0acc462ad7c2b3ffbc7491aac
AlexD <80286400+Alexdlc5@users.noreply.github.com>
2022-06-06 09:35:08 -0700

    .98.5

--- Protoype_Game/Assets/Scripts/Player/Utilities/StabalizePllatform.cs
+++ Protoype_Game/Assets/Scripts/Player/Utilities/StabalizePllatform.cs
@@ -10,7 +10,6 @@ public class StabalizePllatform : MonoBehaviour
         //balances object with y rotation of weapon
         transform.rotation = Quaternion.Euler(0f, weapon.transform.rotation.eulerAngles.y, 0f);
         float Y_transform_delay_percent = .95f;
-        float base_offset = 1;
-        transform.localPosition = new Vector3(weapon.transform.position.x, weapon.transform.position.y * Y_transform_delay_percent + base_offset, weapon.transform.position.z);
+        transform.localPosition = new Vector3(weapon.transform.position.x, weapon.transform.position.y * Y_transform_delay_percent, weapon.transform.position.z);
     }
 }
